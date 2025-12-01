8183badbd0bae5d3f08890da6cea58e69b222890
Flávio Rodrigues <azflavio08@gmail.com>
2025-12-01 12:19:14 -0300

    Delete criacao/backup/backup/monitoramento/list_tables_sizes.sql

--- criacao/backup/backup/monitoramento/list_tables_sizes.sql
+++
@@ -1,5 +0,0 @@
-SELECT table_name AS Tabela,
-       ROUND((data_length + index_length) / 1024 / 1024, 2) AS Tamanho_MB
-FROM information_schema.tables
-WHERE table_schema = 'empresa_db'
-ORDER BY Tamanho_MB DESC;
